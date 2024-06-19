043ceb6d2308570d371437dcd26cc9e700111d39
beep615 <108011774+sok2176@users.noreply.github.com>
2024-06-19 21:14:47 +0300

    T9.py

--- T9.py
+++ T9.py
@@ -4,11 +4,11 @@ class Tnine:
     output = 'PGL Guy: No clue. Geting bot ready to learn. And you, user, get ready to teach!'
     #output = None
     def __init__(self, word:str, dictionary:dict, accuracy=0.5):
-        
-        matching =0 
+
+        matching =0
 
         #print(word)
-        
+
         for key in dictionary:
             l = list(key)
             #print(len(l))
@@ -20,11 +20,10 @@ class Tnine:
                 for i in range(len(list(word))):
                     if l[i]==a[i]:
                        matching+=1
-           
+
                        #print(matching, ' letters matched ')
                        #print()
                        if matching == len(word):                           #print(word, 'replaced',key)
                            self.output = key
-                           print(self.output)
-                           print(f'PGL Guy: did you mean : {self.output}?')
+
                            break
